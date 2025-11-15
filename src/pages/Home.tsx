6f85ec95d0fbbe444084da08ab026da9a1f8c0a3
KongHou <2784332934@qq.com>
2025-11-15 20:40:51 +0800

    feat: add react-router & TailwindCSS as dependencies

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className='px-4 py-8 w-full h-full flex items-center justify-center'>
+      <div className='text-5xl font-bold text-gray-900 mb-6'>
+        Welcome to Kong Player
+      </div>
+    </div>
+  );
+};
+
+export default Home;
